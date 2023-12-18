c18a658d92bbfb089b551eef0babd34a82e349cb
Stroller15 <shubhamverma6351@gmail.com>
2023-12-18 17:35:41 +0530

    add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'Metrica',
+  description: 'A all in one dashboard for your customer',
+}
 
 export default function RootLayout({
   children,
