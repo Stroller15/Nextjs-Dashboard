6391871ac2b8f653069d94c5bb0ca037c7e6f1c7
Stroller15 <shubhamverma6351@gmail.com>
2023-12-20 11:03:30 +0530

    implement fetchRevenue, fetchLatestInvoices, fetchCardData component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
--- scripts/seed.js
+++ scripts/seed.js
@@ -4,7 +4,7 @@ const {
   customers,
   revenue,
   users,
-} = require('../app/lib/placeholder-data.js');
+} = require('../app/lib/placeholder-data.js'); 
 const bcrypt = require('bcrypt');
 
 async function seedUsers(client) {
