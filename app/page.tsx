c70fd852ffef09af24282342a6c30447e52edbb8
Shubham Verma <84174011+Stroller15@users.noreply.github.com>
2025-01-13 01:01:49 +0530

    change dashboard link

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,10 @@ export default function Page() {
             <span>for managining your customer</span>
           </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
